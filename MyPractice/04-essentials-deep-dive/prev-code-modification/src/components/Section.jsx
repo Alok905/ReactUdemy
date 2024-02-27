61680e6b3ccf0c37a4ae3b8a590033fd764044ef
Alok905 <alokranjanjoshi07567@gmail.com>
2024-02-28 00:33:15 +0530

    d

--- MyPractice/04-essentials-deep-dive/prev-code-modification/src/components/Section.jsx
+++
@@ -1,9 +0,0 @@
-export default function Section({ title, children, ...props }) {
-  // ...props it is props forwarding. But all the prop names should be valid attribute name for html as here section tag is assigned with those property that are passed as it is.
-  return (
-    <section {...props}>
-      <h2>{title}</h2>
-      {children}
-    </section>
-  );
-}
